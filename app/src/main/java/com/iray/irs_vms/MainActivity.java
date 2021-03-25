88e853536211a2b5ae397cd45b66591ef3bee18a
gy <timoc1@163.com>
2021-03-25 11:39:58 +0800

    首页banner

--- app/src/main/java/com/iray/irs_vms/MainActivity.java
+++ app/src/main/java/com/iray/irs_vms/MainActivity.java
@@ -2,17 +2,79 @@ package com.iray.irs_vms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.zhouwei.mzbanner.MZBannerView;
+import com.zhouwei.mzbanner.holder.MZHolderCreator;
+import com.zhouwei.mzbanner.holder.MZViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    MZBannerView mzBannerView;
+    MZBannerView mMZBanner;
+    List<Integer> list;
+    public static final int []RES = new int[]{R.mipmap.banner_image1,R.mipmap.banner_image2,R.mipmap.banner_image3,R.mipmap.banner_image4,R.mipmap.banner_image5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBanner();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMZBanner.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMZBanner.pause();
+    }
+
+    private void initBanner(){
+        mMZBanner = (MZBannerView) findViewById(R.id.banner);
+        mMZBanner.setIndicatorVisible(false);
+        list = new ArrayList<>();
+        for(int i=0;i<RES.length;i++){
+            list.add(RES[i]);
+        }
+        // 设置数据
+        mMZBanner.setPages(list, new MZHolderCreator<BannerViewHolder>() {
+            @Override
+            public BannerViewHolder createViewHolder() {
+                return new BannerViewHolder();
+            }
+        });
+    }
+
+    public static class BannerViewHolder implements MZViewHolder<Integer> {
+        private ImageView mImageView;
+        @Override
+        public View createView(Context context) {
+            // 返回页面布局
+            View view = LayoutInflater.from(context).inflate(R.layout.banner_item,null);
+            mImageView = (ImageView) view.findViewById(R.id.banner_image);
+            return view;
+        }
+
+        @Override
+        public void onBind(Context context, int position, Integer data) {
+            // 数据绑定
+            mImageView.setImageResource(data);
+        }
+    }
+
+
+
+
+
 }
